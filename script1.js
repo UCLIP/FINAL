bfbf2d33cb90d0d7e097b694dabc568dee17ca01
UCLIP <75576763+UCLIP@users.noreply.github.com>
2021-05-21 14:31:16 +0100

    Update script1.js

--- script1.js
+++ script1.js
@@ -11,18 +11,7 @@ document.getElementById("futur").onclick = function () {
     location.href = "futur/index.html";
 };
 
-setTimeout(couleur, 4000);
-setTimeout(couleurtxt, 4000);
-function couleur()
-{
-    color=prompt("entrez la couleur que vous voulez en anglais pour le site ");
-    document.body.style.backgroundColor =color;
-}
-function couleurtxt()
-{
-    colortxt=prompt("entrez la couleur que vous voulez en anglais pour le texte");
-    document.body.style.color =colortxt;
-}
+
 let date=Date();
 document.write("votre date de connection est : "+ date);
 
